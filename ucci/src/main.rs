b175d6cac48cfab0a78202f44342e3ba242c2453
Xiaobin Li <l_xb@foxmail.com>
2020-04-02 22:33:22 +0800

    enum & std::io

--- ucci/src/main.rs
+++ ucci/src/main.rs
@@ -1,11 +1,31 @@
+use std::io;
+use std::io::BufRead;
+
 mod ucci;
 
-fn main() {
+fn main() -> io::Result<()> {
     let opt = ucci::Opt {
         n: "bookfiles",
-        t: "string",
+        t: ucci::OpType::STRING,
         v: String::from("v"),
         d: "",
     };
     println!("{}", opt);
+
+    loop {
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        handle.read_line(&mut buffer)?;
+        let cmd = buffer.trim();
+        match cmd {
+            "quit" => {
+                println!("bye");
+                break;
+            }
+            _ => println!("{}", cmd),
+        }
+    }
+
+    Ok(())
 }
--- ucci/src/ucci.rs
+++ ucci/src/ucci.rs
@@ -1,13 +1,37 @@
+use std::fmt;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum OpType {
+    CHECK,
+    STRING,
+    SPIN,
+    COMBO,
+    BUTTON,
+}
+
 #[derive(Debug)]
 pub struct Opt {
     pub n: &'static str,
-    pub t: &'static str,
+    pub t: OpType,
     pub v: String,
     pub d: &'static str,
 }
 
-impl std::fmt::Display for Opt {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl fmt::Display for Opt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "option {} {} default {}", self.n, self.t, self.d)
+    }
+}
+
+impl fmt::Display for OpType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            OpType::CHECK => write!(f, "check"),
+            OpType::STRING => write!(f, "string"),
+            OpType::SPIN => write!(f, "spin"),
+            OpType::COMBO => write!(f, "combo"),
+            OpType::BUTTON => write!(f, "button"),
+        }
     }
 }
